3152167e60923e2111281eae67934ef96023f7f2
Dmitriy_Mk <Dmitriy.mkrtumov@gmail.com>
2025-02-11 16:36:23 +0400

    first commit

--- NetworkingMasterclass/ContentView.swift
+++ NetworkingMasterclass/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let viewModel = CoinsViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
+            
         }
         .padding()
     }
