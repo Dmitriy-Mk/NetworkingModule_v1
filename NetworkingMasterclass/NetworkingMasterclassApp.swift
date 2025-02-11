8f0c01df9592f0f4889c01923867de3a16c9a4dc
Dmitriy_Mk <Dmitriy.mkrtumov@gmail.com>
2025-02-11 14:27:38 +0400

    Initial Commit

---
+++ NetworkingMasterclass/NetworkingMasterclassApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingMasterclassApp.swift
+//  NetworkingMasterclass
+//
+//  Created by Dmitry Mkrtumyan on 11.02.25.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingMasterclassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
